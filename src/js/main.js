a434fc4870efb163d8fbbedbe72fcdfdf2aec644
Ildar Meyker <ildar.meyker@gmail.com>
2021-07-12 13:47:14 +0300

    Add lazy loader.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 const $ = (global.$ = global.jQuery = require("jquery"));
 require("bootstrap/js/dist/collapse");
 require("simplebar");
+require("jquery-lazy");
+
+$(".lazy-image img").Lazy({
+	afterLoad: function (element) {
+		$(element).closest(".lazy-image").addClass("inited");
+	},
+});
